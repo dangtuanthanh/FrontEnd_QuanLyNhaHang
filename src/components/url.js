bfced03ac842a21a62a2e2a76939f7ef6222d61c
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2023-11-22 20:33:09 +0700

    cập nhật file url .js

---
+++ src/components/url.js
@@ -0,0 +1,25 @@
+/* Hệ thống
+ */
+//kiểm tra phiên đăng nhập
+export const urlCheckLogin = 'https://vres.onrender.com/session';
+//đăng nhập
+export const urlLogin = 'https://vres.onrender.com/login';
+//đăng xuất
+export const urlLogout = 'https://vres.onrender.com/logout';
+
+/* Quản lý nhân viên
+ */
+//lấy danh sách nhân viên
+export const urlGetAccount = 'https://vres.onrender.com/getAccount';
+// thêm nhân viên
+export const urlInsertAccount = 'https://vres.onrender.com/insertAccount';
+// sửa nhân viên
+export const urlUpdateAccount = 'https://vres.onrender.com/updateAccount';
+// xoá nhân viên
+export const urlDeleteAccount = 'https://vres.onrender.com/deleteAccount';
+
+//lấy danh sách vai trò
+export const urlGetRole = 'https://vres.onrender.com/getRole';
+
+//lấy danh sách vị trí công việc
+export const urlGetJobPosition = 'https://vres.onrender.com/getJobPosition';

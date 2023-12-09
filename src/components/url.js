5136236e39f6c7bda58092cdef7470defaa6ac8a
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2023-12-09 16:47:55 +0700

    Add files via upload
    update url.js

---
+++ src/components/url.js
@@ -0,0 +1,46 @@
+//https://vres.onrender.com
+/* Hệ thống
+ */
+//kiểm tra phiên đăng nhập
+export const urlCheckLogin = 'https://vres.onrender.com/session';
+//đăng nhập
+export const urlLogin = 'https://vres.onrender.com/login';
+//đăng xuất
+export const urlLogout = 'https://vres.onrender.com/logout';
+
+/* Quản lý nhân viên
+ */
+//lấy danh sách nhân viên
+export const urlGetAccount = 'https://vres.onrender.com/getAccount';
+// thêm nhân viên
+export const urlInsertAccount = 'https://vres.onrender.com/insertAccount';
+// sửa nhân viên
+export const urlUpdateAccount = 'https://vres.onrender.com/updateAccount';
+// xoá nhân viên
+export const urlDeleteAccount = 'https://vres.onrender.com/deleteAccount';
+// undo delete nhân viên
+export const urlUndoDeleteAccount = 'https://vres.onrender.com/undoDeleteAccount';
+// nhập nhân viên
+export const urlImportExcelAccount = 'https://vres.onrender.com/importExcelAccount';
+
+
+//lấy danh sách vai trò
+export const urlGetRole = 'https://vres.onrender.com/getRole';
+// thêm vai trò
+export const urlInsertRole = 'https://vres.onrender.com/insertRole';
+// sửa nhân viên
+export const urlUpdateRole = 'https://vres.onrender.com/updateRole';
+// xoá vai trò
+export const urlDeleteRole = 'https://vres.onrender.com/deleteRole';
+//lấy danh sách quyền
+export const urlGetPermission = 'https://vres.onrender.com/getPermission';
+
+
+//lấy danh sách vị trí công việc
+export const urlGetJobPosition = 'https://vres.onrender.com/getJobPosition';
+// thêm vị trí công việc
+export const urlInsertJobPosition = 'https://vres.onrender.com/insertJobPosition';
+// sửa vị trí công việc
+export const urlUpdateJobPosition = 'https://vres.onrender.com/updateJobPosition';
+//xoá vị trí công việc
+export const urlDeleteJobPosition = 'https://vres.onrender.com/deleteJobPosition';

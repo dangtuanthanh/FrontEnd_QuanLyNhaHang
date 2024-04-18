8b0c6bd81169dcf64f134cf4726d2f3e5b9c00da
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2024-04-18 16:20:59 +0700

    update_url.js
    update_url.js

--- src/components/url.js
+++ src/components/url.js
@@ -1,200 +1,200 @@
-//https://vres.onrender.com
+//https://benh.onrender.com
 /* Hệ thống
  */
 //kiểm tra phiên đăng nhập
-export const urlCheckLogin = 'https://vres.onrender.com/session';
+export const urlCheckLogin = 'https://benh.onrender.com/session';
 //đăng nhập
-export const urlLogin = 'https://vres.onrender.com/login';
+export const urlLogin = 'https://benh.onrender.com/login';
 //đăng xuất
-export const urlLogout = 'https://vres.onrender.com/logout';
+export const urlLogout = 'https://benh.onrender.com/logout';
 //đổi mật khẩu
-export const urlChangePassword = 'https://vres.onrender.com/changePassword';
+export const urlChangePassword = 'https://benh.onrender.com/changePassword';
 
 /* Quản lý nhân viên
  */
 //lấy danh sách nhân viên
-export const urlGetAccount = 'https://vres.onrender.com/getAccount';
+export const urlGetAccount = 'https://benh.onrender.com/getAccount';
 // thêm nhân viên
-export const urlInsertAccount = 'https://vres.onrender.com/insertAccount';
+export const urlInsertAccount = 'https://benh.onrender.com/insertAccount';
 // sửa nhân viên
-export const urlUpdateAccount = 'https://vres.onrender.com/updateAccount';
+export const urlUpdateAccount = 'https://benh.onrender.com/updateAccount';
 // xoá nhân viên
-export const urlDeleteAccount = 'https://vres.onrender.com/deleteAccount';
+export const urlDeleteAccount = 'https://benh.onrender.com/deleteAccount';
 // undo delete nhân viên
-export const urlUndoDeleteAccount = 'https://vres.onrender.com/undoDeleteAccount';
+export const urlUndoDeleteAccount = 'https://benh.onrender.com/undoDeleteAccount';
 // nhập nhân viên
-export const urlImportExcelAccount = 'https://vres.onrender.com/importExcelAccount';
+export const urlImportExcelAccount = 'https://benh.onrender.com/importExcelAccount';
 
 
 //lấy danh sách vai trò
-export const urlGetRole = 'https://vres.onrender.com/getRole';
+export const urlGetRole = 'https://benh.onrender.com/getRole';
 // thêm vai trò
-export const urlInsertRole = 'https://vres.onrender.com/insertRole';
+export const urlInsertRole = 'https://benh.onrender.com/insertRole';
 // sửa nhân viên
-export const urlUpdateRole = 'https://vres.onrender.com/updateRole';
+export const urlUpdateRole = 'https://benh.onrender.com/updateRole';
 // xoá vai trò
-export const urlDeleteRole = 'https://vres.onrender.com/deleteRole';
+export const urlDeleteRole = 'https://benh.onrender.com/deleteRole';
 //lấy danh sách quyền
-export const urlGetPermission = 'https://vres.onrender.com/getPermission';
+export const urlGetPermission = 'https://benh.onrender.com/getPermission';
 
 
 //lấy danh sách vị trí công việc
-export const urlGetJobPosition = 'https://vres.onrender.com/getJobPosition';
+export const urlGetJobPosition = 'https://benh.onrender.com/getJobPosition';
 // thêm vị trí công việc
-export const urlInsertJobPosition = 'https://vres.onrender.com/insertJobPosition';
+export const urlInsertJobPosition = 'https://benh.onrender.com/insertJobPosition';
 // sửa vị trí công việc
-export const urlUpdateJobPosition = 'https://vres.onrender.com/updateJobPosition';
+export const urlUpdateJobPosition = 'https://benh.onrender.com/updateJobPosition';
 //xoá vị trí công việc
-export const urlDeleteJobPosition = 'https://vres.onrender.com/deleteJobPosition';
+export const urlDeleteJobPosition = 'https://benh.onrender.com/deleteJobPosition';
 
 
 /* Quản lý bàn và khu vực
  */
 //lấy danh sách khu vực
-export const urlGetArea = 'https://vres.onrender.com/getArea';
+export const urlGetArea = 'https://benh.onrender.com/getArea';
 // thêm Khu vực
-export const urlInsertArea = 'https://vres.onrender.com/insertArea';
+export const urlInsertArea = 'https://benh.onrender.com/insertArea';
 // sửa khu vực
-export const urlUpdateArea = 'https://vres.onrender.com/updateArea';
+export const urlUpdateArea = 'https://benh.onrender.com/updateArea';
 //xoá vị trí khu vực
-export const urlDeleteArea = 'https://vres.onrender.com/deleteArea';
+export const urlDeleteArea = 'https://benh.onrender.com/deleteArea';
 
 //lấy danh sách bàn
-export const urlGetTable = 'https://vres.onrender.com/getTable';
+export const urlGetTable = 'https://benh.onrender.com/getTable';
 // thêm bàn
-export const urlInsertTable = 'https://vres.onrender.com/insertTable';
+export const urlInsertTable = 'https://benh.onrender.com/insertTable';
 // sửa bàn
-export const urlUpdateTable = 'https://vres.onrender.com/updateTable';
+export const urlUpdateTable = 'https://benh.onrender.com/updateTable';
 //xoá bàn
-export const urlDeleteTable = 'https://vres.onrender.com/deleteTable';
+export const urlDeleteTable = 'https://benh.onrender.com/deleteTable';
 
 /* Quản lý ca làm việc
  */
 //lấy danh sách ca làm việc
-export const urlGetShifts = 'https://vres.onrender.com/getShifts';
+export const urlGetShifts = 'https://benh.onrender.com/getShifts';
 // thêm ca làm việc
-export const urlInsertShifts = 'https://vres.onrender.com/insertShifts';
+export const urlInsertShifts = 'https://benh.onrender.com/insertShifts';
 // sửa ca làm việc
-export const urlUpdateShifts = 'https://vres.onrender.com/updateShifts';
+export const urlUpdateShifts = 'https://benh.onrender.com/updateShifts';
 //xoá ca làm việc
-export const urlDeleteShifts = 'https://vres.onrender.com/deleteShifts';
+export const urlDeleteShifts = 'https://benh.onrender.com/deleteShifts';
 
 //lấy danh sách chốt ca
-export const urlGetCloseShifts = 'https://vres.onrender.com/getCloseShifts';
+export const urlGetCloseShifts = 'https://benh.onrender.com/getCloseShifts';
 //tải danh sách ca phù hợp với giờ hiện tại
-export const urlGetMatchShifts = 'https://vres.onrender.com/GetMatchShifts';
+export const urlGetMatchShifts = 'https://benh.onrender.com/GetMatchShifts';
 // thêm chốt ca mới
-export const urlInsertCloseShifts = 'https://vres.onrender.com/insertCloseShifts';
+export const urlInsertCloseShifts = 'https://benh.onrender.com/insertCloseShifts';
 // cập nhật chốt ca
-export const urlUpdateCloseShifts = 'https://vres.onrender.com/updateCloseShifts';
+export const urlUpdateCloseShifts = 'https://benh.onrender.com/updateCloseShifts';
 // xoá chốt ca
-export const urlDeleteCloseShifts = 'https://vres.onrender.com/deleteCloseShifts';
+export const urlDeleteCloseShifts = 'https://benh.onrender.com/deleteCloseShifts';
 
 
 /* Quản lý Khách Hàng
  */
 //lấy danh sách khách hàng
-export const urlGetCustomer = 'https://vres.onrender.com/getCustomer';
+export const urlGetCustomer = 'https://benh.onrender.com/getCustomer';
 // thêm khách hàng
-export const urlInsertCustomer = 'https://vres.onrender.com/insertCustomer';
+export const urlInsertCustomer = 'https://benh.onrender.com/insertCustomer';
 // sửa khách hàng
-export const urlUpdateCustomer = 'https://vres.onrender.com/updateCustomer';
+export const urlUpdateCustomer = 'https://benh.onrender.com/updateCustomer';
 //xoá khách hàng
-export const urlDeleteCustomer = 'https://vres.onrender.com/deleteCustomer';
+export const urlDeleteCustomer = 'https://benh.onrender.com/deleteCustomer';
 
 
 /* Quản lý Kho
  */
 //lấy danh sách đơn vị tính
-export const urlGetUnit = 'https://vres.onrender.com/getUnit';
+export const urlGetUnit = 'https://benh.onrender.com/getUnit';
 //lấy danh sách chuyển đổi đơn vị tính
-export const urlGetListUnitConversions = 'https://vres.onrender.com/getListUnitConversions';
+export const urlGetListUnitConversions = 'https://benh.onrender.com/getListUnitConversions';
 //lấy danh sách chuyển đổi đơn vị tính theo ID
-export const urlGetListUnitConversionsByIDUnit = 'https://vres.onrender.com/getListUnitConversionsByIDUnit';
+export const urlGetListUnitConversionsByIDUnit = 'https://benh.onrender.com/getListUnitConversionsByIDUnit';
 
 // thêm đơn vị tính
-export const urlInsertUnit = 'https://vres.onrender.com/insertUnit';
+export const urlInsertUnit = 'https://benh.onrender.com/insertUnit';
 // sửa đơn vị tính
-export const urlUpdateUnit = 'https://vres.onrender.com/updateUnit';
+export const urlUpdateUnit = 'https://benh.onrender.com/updateUnit';
 //xoá đơn vị tính
-export const urlDeleteUnit = 'https://vres.onrender.com/deleteUnit';
+export const urlDeleteUnit = 'https://benh.onrender.com/deleteUnit';
 
 //lấy danh sách phiếu nhập
-export const urlGetReceipt = 'https://vres.onrender.com/getReceipt';
+export const urlGetReceipt = 'https://benh.onrender.com/getReceipt';
 // thêm phiếu nhập
-export const urlInsertReceipt = 'https://vres.onrender.com/insertReceipt';
+export const urlInsertReceipt = 'https://benh.onrender.com/insertReceipt';
 // sửa phiếu nhập
-export const urlUpdateReceipt = 'https://vres.onrender.com/updateReceipt';
+export const urlUpdateReceipt = 'https://benh.onrender.com/updateReceipt';
 //xoá phiếu nhập
-export const urlDeleteReceipt = 'https://vres.onrender.com/deleteReceipt';
+export const urlDeleteReceipt = 'https://benh.onrender.com/deleteReceipt';
 
 //lấy danh sách nguyên liệu
-export const urlGetIngredient = 'https://vres.onrender.com/getIngredient';
+export const urlGetIngredient = 'https://benh.onrender.com/getIngredient';
 // thêm nguyên liệu
-export const urlInsertIngredient = 'https://vres.onrender.com/insertIngredient';
+export const urlInsertIngredient = 'https://benh.onrender.com/insertIngredient';
 // sửa nguyên liệu
-export const urlUpdateIngredient = 'https://vres.onrender.com/updateIngredient';
+export const urlUpdateIngredient = 'https://benh.onrender.com/updateIngredient';
 //xoá nguyên liệu
-export const urlDeleteIngredient = 'https://vres.onrender.com/deleteIngredient';
+export const urlDeleteIngredient = 'https://benh.onrender.com/deleteIngredient';
 
 
 /* Quản lý Thực đơn
  */
 //lấy danh sách toàn bộ sản phẩm 
-export const urlGetProduct = 'https://vres.onrender.com/getProduct';
+export const urlGetProduct = 'https://benh.onrender.com/getProduct';
 //xoá sản phẩm
-export const urlDeleteProduct = 'https://vres.onrender.com/deleteProduct';
+export const urlDeleteProduct = 'https://benh.onrender.com/deleteProduct';
 
 
 // thêm sản phẩm thành phẩm
-export const urlInsertFinishedProduct = 'https://vres.onrender.com/insertFinishedProduct';
+export const urlInsertFinishedProduct = 'https://benh.onrender.com/insertFinishedProduct';
 // sửa sản phẩm thành phẩm
-export const urlUpdateFinishedProduct = 'https://vres.onrender.com/updateFinishedProduct';
+export const urlUpdateFinishedProduct = 'https://benh.onrender.com/updateFinishedProduct';
 // thêm sản phẩm chế biến
-export const urlInsertProcessedProduct = 'https://vres.onrender.com/insertProcessedProduct';
+export const urlInsertProcessedProduct = 'https://benh.onrender.com/insertProcessedProduct';
 // sửa sản phẩm chế biến
-export const urlUpdateProcessedProduct = 'https://vres.onrender.com/updateProcessedProduct';
+export const urlUpdateProcessedProduct = 'https://benh.onrender.com/updateProcessedProduct';
 
 //lấy danh sách loại sản phẩm
-export const urlGetTypeProduct = 'https://vres.onrender.com/getTypeProduct';
+export const urlGetTypeProduct = 'https://benh.onrender.com/getTypeProduct';
 // thêm loại sản phẩm
-export const urlInsertTypeProduct = 'https://vres.onrender.com/insertTypeProduct';
+export const urlInsertTypeProduct = 'https://benh.onrender.com/insertTypeProduct';
 // sửa loại sản phẩm
-export const urlUpdateTypeProduct = 'https://vres.onrender.com/updateTypeProduct';
+export const urlUpdateTypeProduct = 'https://benh.onrender.com/updateTypeProduct';
 //xoá loại sản phẩm
-export const urlDeleteTypeProduct = 'https://vres.onrender.com/deleteTypeProduct';
+export const urlDeleteTypeProduct = 'https://benh.onrender.com/deleteTypeProduct';
 
 /* Quản lý Hoá Đơn
  */
 //lấy danh sách hoá đơn
-export const urlGetInvoice = 'https://vres.onrender.com/getInvoice';
+export const urlGetInvoice = 'https://benh.onrender.com/getInvoice';
 // thêm hoá đơn
-export const urlInsertInvoice = 'https://vres.onrender.com/insertInvoice';
+export const urlInsertInvoice = 'https://benh.onrender.com/insertInvoice';
 // sửa hoá đơn
-export const urlUpdateInvoice = 'https://vres.onrender.com/updateInvoice';
+export const urlUpdateInvoice = 'https://benh.onrender.com/updateInvoice';
 //xoá hoá đơn
-export const urlDeleteInvoice = 'https://vres.onrender.com/deleteInvoice';
+export const urlDeleteInvoice = 'https://benh.onrender.com/deleteInvoice';
 // cập nhật trạng thái bàn ăn
-export const urlUpdateStatusTable = 'https://vres.onrender.com/updateStatusTable';
+export const urlUpdateStatusTable = 'https://benh.onrender.com/updateStatusTable';
 // lấy ảnh thanh toán
-export const urlGetPicturePayment = 'https://vres.onrender.com/getPicturePayment';
+export const urlGetPicturePayment = 'https://benh.onrender.com/getPicturePayment';
 
 /* Quản lý Bếp
  */
 //lấy danh sách order
-export const urlGetOrder = 'https://vres.onrender.com/getListProductsByStatus';
+export const urlGetOrder = 'https://benh.onrender.com/getListProductsByStatus';
 //cập nhật trạng thái món ăn
-export const urlUpdateStatusProduct = 'https://vres.onrender.com/updateStatusProduct';
+export const urlUpdateStatusProduct = 'https://benh.onrender.com/updateStatusProduct';
 
 /* Bảng điều khiển
  */
 //lấy số bàn đang có khách
-export const urlGetOccupiedTables = 'https://vres.onrender.com/getOccupiedTables';
+export const urlGetOccupiedTables = 'https://benh.onrender.com/getOccupiedTables';
 //lấy số hoá đơn trong ngày
-export const urlGetInvoiceToday = 'https://vres.onrender.com/getInvoiceToday';
+export const urlGetInvoiceToday = 'https://benh.onrender.com/getInvoiceToday';
 //lấy tổng tiền hoá đơn trong ngày
-export const urlGetRevenueToday = 'https://vres.onrender.com/getRevenueToday';
+export const urlGetRevenueToday = 'https://benh.onrender.com/getRevenueToday';
 //lấy tổng tiền hoá đơn trong tháng
-export const urlGetRevenueMonth = 'https://vres.onrender.com/getRevenueMonth';
+export const urlGetRevenueMonth = 'https://benh.onrender.com/getRevenueMonth';
 //lấy tổng tiền hoá đơn trong tuần
-export const urlGetListRevenueMonth = 'https://vres.onrender.com/getListRevenueMonth';
+export const urlGetListRevenueMonth = 'https://benh.onrender.com/getListRevenueMonth';
